4874ca37c379946cdace6c3e2e89fb28dfb4ef54
Victoria Volochay <vvolochay@gmail.com>
2023-03-28 03:51:13 +0300

    vsosh data parsers

--- src/main/kotlin/com/vvolochay/PersonalAchievementsGen.kt
+++ src/main/kotlin/com/vvolochay/PersonalAchievementsGen.kt
@@ -2,7 +2,14 @@ package com.vvolochay
 
 import java.io.File
 
-data class ContestantData(val id: String, val name: String, val region: String, val additionalInfo: String)
+data class ContestantData(
+    val id: String,
+    val name: String,
+    val region: String,
+    val additionalInfo: String,
+    val diploma: String,
+    val points: String
+)
 
 class PersonalAchievementsGen : Generator() {
     override fun run(filename: File, logo: File, template: File, outputDir: String, result: Boolean) {
@@ -12,18 +19,35 @@ class PersonalAchievementsGen : Generator() {
 
         filename.forEachLine {
             val info = parseLines(it)
-            val replaced = template.readText(Charsets.UTF_8)
+            var replaced = template.readText(Charsets.UTF_8)
                 .replace("{Name}", info.name)
-                .replace("{Region}", info.region)
-                .replace("{additionalInfo}", info.additionalInfo)
                 .replace("{Logo}", base64Logo(if (logo.isDirectory) File(logo.path + "/" + info.id + ".png") else logo))
+
+            replaced = if (result) {
+                replaceVsoshData(replaced, info.points)
+                    .replace("{Region}", "${info.additionalInfo}, ${info.region}")
+                    .replace("{additionalInfo}", info.diploma)
+            } else {
+                replaced.replace("{Region}", info.region)
+                    .replace("{additionalInfo}", info.additionalInfo)
+            }
 
             File("$outputDir/${info.id}.svg").writeText(replaced, Charsets.UTF_8)
         }
     }
 
+    private fun replaceVsoshData(s: String, p: String): String {
+        val points = p.split("\t").map { i -> i.toInt() }
+        if (points.size != 9) throw IllegalArgumentException("Not enough points in input, expected 9, but actual ${points.size}")
+        return s
+            .replace("{d1}", points.subList(0, 4).sum().toString())
+            .replace("{d2}", points.subList(4, 8).sum().toString())
+            .replace("{sum}", points.last().toString())
+    }
+
     private fun parseLines(str: String): ContestantData {
         val data = str.split("|").map { s -> s.trim() }
-        return ContestantData(data[0], data[1], data[2], data[3])
+        return if (data.size == 4) ContestantData(data[0], data[1], data[2], data[3], "", "")
+        else ContestantData(data[0], data[1], data[2], data[3], data[4], data[5])
     }
 }
---
+++ vsosh_res2022.bat
@@ -0,0 +1,1 @@
+java -jar build/libs/ach-gen.jar -i data/vsosh2022/vsosh_res.txt  --svg data/vsosh2022/achievement_result.svg -t person
