ec5dcb96e2b9683ccb5e3ccfa4837d69f25eec6b
Victoria Volochay <vvolochay@gmail.com>
2023-04-01 15:31:30 +0300

    vsosh 2023

--- vsosh2022.bat
+++
@@ -1,1 +0,0 @@
-java -jar build/libs/ach-gen.jar -i data/vsosh2022/vsosh.txt  --svg data/vsosh2022/achievement.svg -t person
---
+++ vsosh2023.bat
@@ -0,0 +1,1 @@
+java -jar build/libs/ach-gen.jar -i data/vsosh2023/data.txt  --svg data/vsosh2023/achievement.svg -t person
