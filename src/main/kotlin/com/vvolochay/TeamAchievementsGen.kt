c481ddaff2a5dedfc0bc454fc407b49b93c2a6b8
Victoria Volochay <vvolochay@gmail.com>
2023-11-26 12:30:03 +0300

    * nwrrc

--- src/main/kotlin/com/vvolochay/TeamAchievementsGen.kt
+++ src/main/kotlin/com/vvolochay/TeamAchievementsGen.kt
@@ -16,7 +16,7 @@ class TeamAchievementsGen : Generator() {
                 .replace("{University}", teamInfo.university)
                 .replace("{HashTag}", teamInfo.hashTag)
                 .replace("{Contestants}", teamInfo.contestants)
-                .replace("{TeamName}", teamInfo.teamName)
+                .replace("{TeamName}", replaceEscapingSymbols(teamInfo.teamName))
                 .replace("{Logo}",
                     base64Logo(if (logo.isDirectory) File(logo.path + "/" + teamInfo.id + ".png") else logo))
             File("$outputDir/${teamInfo.id}.svg").writeText(replaced, Charsets.UTF_8)
@@ -26,5 +26,9 @@ class TeamAchievementsGen : Generator() {
     private fun parseData(str: String): TeamData {
         val teamData = str.split("|").map { s -> s.trim() }
         return TeamData(teamData[0], teamData[1], teamData[2], teamData[3], teamData[4])
+    }
+
+    fun replaceEscapingSymbols(str: String): String {
+        return str.replace("&", "&amp;").replace("<", "&#60;")
     }
 }
